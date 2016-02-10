2bad3c825dc9c40f3d6fbd7cbaeeaee5746984b6
Jolja06 <elena.an.92@gmail.com>
2016-02-11 01:08:22 +0300

    validat on php

--- app/js/popup.js
+++ app/js/popup.js
@@ -1,15 +1,61 @@
-;(function() {
+var myModule = (function () {
 
-	$(function(){
-		$('#popup-add').on('click', function(e){
-			e.preventDefault();
+	// Инициализирует наш модуль
+	function init () {
+		_setUpListners();
+	};
 
-			$('#add-project').bPopup({
-				transition : 'slideUp',
-				transitionClose : 'slideDown',
-				speed : 500
-			});
+	// Прослушивает события 
+	function _setUpListners () {
+		$('#popup-add').on('click', _showModal); //открыть модальное окно
+		$('#add-project-form').on('submit', _addProject); //добавление проекта
+	}
+
+	function _showModal (e) {
+		e.preventDefault();
+		
+		$('#add-project').bPopup({
+			transition : 'slideUp',
+			transitionClose : 'slideDown',
+			speed : 500
 		});
-	});
+	}
+
+	function _addProject (e) {
+		e.preventDefault();
+		console.log('Add progect');
+
+		var form = $(this), //теперь это jQuery объект 
+			url = 'add_project.php',
+			data = form.serialize();
+
+		console.log(form);
+
+		$.ajax({
+			url: url,
+			type: 'POST',
+			dataType: 'json',
+			data: data,
+		})
+		.done(function(answer) {
+			console.log("success");
+			console.log(answer);
+		})
+		.fail(function() {
+			console.log("error");
+		})
+		.always(function() {
+			console.log("complete");
+		});
+		
+	}
+
+	// Возвращаем объект (публичные методы) 
+	return {
+		init: init
+	};
 
 })();
+
+// Вызов модуля
+myModule.init();
